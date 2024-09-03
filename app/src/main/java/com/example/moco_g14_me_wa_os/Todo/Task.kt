0d011f50410f189772c3e87cf8cc7edc78aee9c3
rasmus.walsh <rasmus.walsh@smail.th-koeln.de>
2024-09-03 13:43:00 +0200

    first stuff

--- app/src/main/java/com/example/moco_g14_me_wa_os/Todo/Task.kt
+++ app/src/main/java/com/example/moco_g14_me_wa_os/Todo/Task.kt
@@ -1,4 +1,10 @@
-package com.example.moco_g14_me_wa_os.Todo;
+package com.example.moco_g14_me_wa_os.Todo
 
-public class Task {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Task (val name: String,
+    val completed: Boolean = false,
+    @PrimaryKey(autoGenerate = true),
+    val taskID: Int = 0)
---
+++ app/src/main/java/com/example/moco_g14_me_wa_os/Todo/TodoState.kt
@@ -0,0 +1,5 @@
+package com.example.moco_g14_me_wa_os.Todo
+
+data class TodoState(val name : String = "",
+                     val completed : Boolean = false,
+                     val isAddingEntry : Boolean = false)
