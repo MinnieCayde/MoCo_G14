1c57bfc7dd058b865650c809cd60ddb789919e28
Amin Merzouk <amin.merzouk@smail.th-koeln.de>
2024-09-05 11:44:31 +0200

    Settingscreen und Test Main Activity

--- app/src/main/java/com/example/moco_g14_me_wa_os/MainActivity.kt
+++ app/src/main/java/com/example/moco_g14_me_wa_os/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.moco_g14_me_wa_os
 
 import android.os.Bundle
+import android.widget.ToggleButton
 import androidx.activity.ComponentActivity
+import android.graphics.Color
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.collection.floatIntMapOf
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -16,15 +19,43 @@ import com.example.moco_g14_me_wa_os.ui.theme.Moco_G14_Me_Wa_OsTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            Moco_G14_Me_Wa_OsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+        setContentView(R.layout.settings_layout)
+
+
+
+        // Referenzen zu den ToggleButtons (3 ersten sind für Session Selection)
+        val toggleOption1 = findViewById<ToggleButton>(R.id.toggleOption1)
+        val toggleOption2 = findViewById<ToggleButton>(R.id.toggleOption2)
+        val toggleOption3 = findViewById<ToggleButton>(R.id.toggleOption3)
+        val worktimeOption1 = findViewById<ToggleButton>(R.id.workTimeOption1)
+        val worktimeOption2 = findViewById<ToggleButton>(R.id.workTimeOption2)
+        val shortBreakTimeOption1 = findViewById<ToggleButton>(R.id.shortBreakTimeOption1)
+        val shortBreakTimeOption2 = findViewById<ToggleButton>(R.id.shortBreakTimeOption2)
+        val longBreakTimeOption1 = findViewById<ToggleButton>(R.id.longBreakTimeOption1)
+        val longBreakTimeOption2 = findViewById<ToggleButton>(R.id.longBreakTimeOption2)
+
+        // Alle ToggleButtons in eine Liste aufnehmen
+        val toggleButtons = listOf(toggleOption1, toggleOption2, toggleOption3)
+
+        // Event Listener für jeden ToggleButton
+        for (toggle in toggleButtons) {
+            toggle.setOnCheckedChangeListener { buttonView, isChecked ->
+                if (isChecked) {
+                    // Nur dieser ToggleButton wird aktiv - alle anderen deaktivieren
+                    deactivateOthers(toggleButtons, buttonView.id)
+                    buttonView.setBackgroundResource(R.drawable.active_rounded_time_selectors)
+                    buttonView.setTextColor(resources.getColor(R.color.white))
+                } else {
+                    buttonView.setBackgroundResource(R.drawable.inactive_rounded_time_selectors)
                 }
+            }
+        }
+    }
+
+    private fun deactivateOthers(toggleButtons: List<ToggleButton>, activeid: Int) {
+        for (toggle in toggleButtons) {
+            if (toggle.id != activeid) {
+                toggle.isChecked = false
             }
         }
     }
