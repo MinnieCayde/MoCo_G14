7fc341a52f8338309af8b3fe3701d8fb15ea8052
Amin Merzouk <amin.merzouk@smail.th-koeln.de>
2024-09-07 16:38:55 +0200

    fürs amins laptop

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,8 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.preference)
     implementation(libs.material)
+    implementation(libs.androidx.navigation.runtime.ktx)
+    implementation(libs.androidx.navigation.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/moco_g14_me_wa_os/MainActivity.kt
+++ app/src/main/java/com/example/moco_g14_me_wa_os/MainActivity.kt
@@ -7,6 +7,7 @@ import android.graphics.Color
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.collection.floatIntMapOf
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -15,12 +16,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.moco_g14_me_wa_os.ui.theme.Moco_G14_Me_Wa_OsTheme
+import kotlinx.serialization.Serializable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_layout)
 
+        /*
         // Referenzen zu den ToggleButtons
         val toggleOption1 = findViewById<ToggleButton>(R.id.toggleOption1)
         val toggleOption2 = findViewById<ToggleButton>(R.id.toggleOption2)
@@ -57,7 +60,8 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
+    */
+    }
 
     @Composable
     fun Greeting(name: String, modifier: Modifier = Modifier) {
@@ -74,5 +78,7 @@ class MainActivity : ComponentActivity() {
             Greeting("Android")
         }
     }
+}
+@Serializable
 
 
